21f6d2952747a748a8c9510703e4e27bd073f367
BONALA VAMSHI KRISHNA CHARY <vamshi.bonala@in.techwave.net>
2025-04-15 15:01:00 +0530

    Added new files to the Local Repo

---
+++ LLD-02-04-2025/02-04/src.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("This is a simple Go program.")
+	fmt.Println("It demonstrates basic syntax and structure.")
+	fmt.Println("You can run this code using the Go compiler.")
+	fmt.Println("Make sure you have Go installed on your machine.")
+	fmt.Println("Happy coding!")
+	fmt.Println("This is the end of the program.")
+	fmt.Println("Goodbye!")
+	fmt.Println("Added new line in new branch. ,Vikash")
+}
