c4ba5eca3b3c894e37805cd7f8961f6bd93e6cbe
BONALA VAMSHI KRISHNA CHARY <vamshi.bonala@in.techwave.net>
2025-09-05 14:52:08 +0530

    Added Java code from previous version

--- LeetCode problems/twoSum.py
+++ LeetCode problems/twoSum.py
@@ -11,3 +11,20 @@ class Solution:
 # Example usage:
 solution = Solution()
 print(solution.twoSum([2, 7, 11, 15], 9))
+
+#Below is the Java version of the same solution
+
+# class Solution {
+#    public int[] twoSum(int[] nums, int target) {
+#        int n = nums.length;
+#        Map<Integer,Integer> numMap = new HashMap<>();
+#        for(int i = 0;i<n;i++){
+#            int com = target - nums[i];
+#            if(numMap.containsKey(com)){
+#                return new int[]{numMap.get(com),i};
+#            }
+#            numMap.put(nums[i], i);
+#       }
+#        return new int[]{};
+#    }
+# }
